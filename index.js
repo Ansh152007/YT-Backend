a24e99c106f96cd12b813786bb528edfaf89d740
Ansh Tiwari <167138279+Ansh152007@users.noreply.github.com>
2024-09-12 03:41:01 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import dotenv from "dotenv";
 import ConnectDB from "./DB/database.js";
 dotenv.config({path:"./config.env"})
-
 ConnectDB
+const PORT = process.env.PORT || 5000;
+
+.then(()=>{
+  app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)}
+})
