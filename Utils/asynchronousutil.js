f654f39bd18bfba7f8b76bfb5da3d085e9cf83cc
Ansh152007 <167138279+Ansh152007@users.noreply.github.com>
2024-09-15 23:17:04 +0530

    update

--- Utils/asynchronousutil.js
+++ Utils/asynchronousutil.js
@@ -1,3 +1,10 @@
-const asynchandler = () =>{}
+import { response } from "express"
+
+
+const asynchandler = (requestHandler) =>(req,res,next)=>{
+Promise.resolve(requestHandler(req,res,next)).catch((err)=>(next(err)))
+}
+
+
 
 export default asynchandler
