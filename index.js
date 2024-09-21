ce5d0707a5a183c0b2fc5355a9727cddbe99d567
Ansh152007 <167138279+Ansh152007@users.noreply.github.com>
2024-09-22 01:02:41 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import ConnectDB from "./DB/database.js";
-
-dotenv.config({path:"./config.env"})
+import app from "./app.js";
+dotenv.config({path:"./.env"})
 const PORT = process.env.PORT || 5000;
 
 ConnectDB()
