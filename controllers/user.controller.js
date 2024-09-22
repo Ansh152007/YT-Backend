eb65c7a011fdbaa973b7f9deeeaf179f526fd0b2
Ansh152007 <167138279+Ansh152007@users.noreply.github.com>
2024-09-22 11:25:04 +0530

    update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,28 @@
-import { json } from "stream/consumers"
+import ApiError from "../Utils/ApiError.js"
 import asynchandler from "../Utils/asynchronousutil.js"
+import User from "../models/user.model.js"
 
 const registerUser = asynchandler( async (req,res)=> {
-    res.status(200).json({
-        message : "ok"
+    // Get data from frontend
+    // Validation - not empty
+    // Check if user already exists: Username,email
+    // Check for images/avatar
+    // Upload to cloudinary
+    // Create user object - create entry in db
+    // Remove password and refresh token field from response
+    // Check for user creation
+    // Return response
+
+const{fullName,username,email,password}= req.body
+    console.log("Email:",email)
+
+    if([fullName,username,email,password].some((field)=> field?.trim() ==="")){
+        throw new ApiError(400,"All fields are required!");
+        
+    }
+
+    User.findOne({
+        $or: [{}]
     })
 })
 
